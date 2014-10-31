63e73becd583ab3848253a91f0beda2dcfbfe458
Marco Salles <marcosalles@gmail.com>
2014-10-31 13:17:19 -0200

    + login and menu improved

--- app/controllers/CardController.java
+++ app/controllers/CardController.java
@@ -3,27 +3,30 @@ package controllers;
 import java.util.List;
 
 import models.magic.Card;
+import play.api.templates.Html;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.Result;
+import scala.collection.mutable.StringBuilder;
 import views.html.card.*;
 
 import com.avaje.ebean.Ebean;
 
 public class CardController extends BaseController {
 
-	public static Result buscaForm() {
-		return ok(searchForm.render());
+	public static Result searchForm() {
+		Form<Card> form = Form.form(Card.class);
+		return wrapOk(searchForm.render(form));
 	}
 
-	public static Result busca() {
+	public static Result search() {
 		DynamicForm form = Form.form().bindFromRequest();
 		String value = form.field("suggestText").value();
 		List<Card> list = Ebean.createQuery(Card.class)
 				.where()
 				.ilike("suggestText", "%"+value+"%")
 				.findList();
-		return ok(Json.toJson(list));
+		return wrapOk(cards.render(list));
 	}
 }
