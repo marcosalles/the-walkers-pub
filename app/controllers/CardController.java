72737c263d22bef6762b85f96e9affd7df663402
Marco Salles <marcosalles@gmail.com>
2014-11-04 10:11:47 -0200

    + card search and list on same page

--- app/controllers/CardController.java
+++ app/controllers/CardController.java
@@ -1,27 +1,26 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import models.magic.Card;
-import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Result;
-import views.html.card.cards;
-import views.html.card.searchForm;
+import views.html.card.searchCards;
 import daos.CardDao;
 
 public class CardController extends BaseController {
 
 	public static Result searchForm() {
 		Form<Card> form = Form.form(Card.class);
-		return wrapOk(searchForm.render(form));
+		return wrapOk(searchCards.render(null, form));
 	}
 
 	public static Result search() {
-		DynamicForm form = Form.form().bindFromRequest();
+		Form<Card> form = Form.form(Card.class).bindFromRequest();
 		String name = form.field("suggestText").value();
 		List<Card> list = CardDao.cardsByName(name);
-		return wrapOk(cards.render(list));
+		return wrapOk(searchCards.render(list, form));
 	}
 
 	public static Result card(String id) {
