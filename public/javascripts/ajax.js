de9e895565798c82118eeff6dd5312266345a0d4
Marco Salles <marcosalles@gmail.com>
2014-12-16 14:42:04 -0200

    + some html and JS bugs

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,13 +1,3 @@
-$("body").on("click", ".toggle", function(e) {
-	var info = $(this).parent().find(".info");
-	if (info.hasClass("hidden")) {
-		info.removeClass("hidden");
-	} else {
-		info.addClass("hidden");
-	}
-	e.preventDefault();
-});
-
 function flash(key, message) {
 	var alert = $("#alert-template").clone();
 	alert.attr("id", "");
@@ -22,22 +12,6 @@ function ajax(url, method, data) {
 		data: data
 	});	
 };
-$(function(){
-	var templates = $(".template").sort(asc_sort);
-	templates.each(function(){
-		var template = $(this);
-		var id = template.attr("id");
-		var url = template.attr("data-url");
-		var method = "get";
-		var result = ajax(url, method, {});
-		result.done(function(data) {
-			template.html(data);
-		});
-		result.fail(function(jqXHR, textStatus, errorThrown) {
-			template.html("error loading "+id+".. "+jqXHR.responseText);
-		});
-	});	
-});
 function asc_sort(a, b){
   return ($(b).attr("data-sort")) < ($(a).attr("data-sort")) ? 1 : -1;    
 }
